82587148f734417d00c6fa1753e5358925599f9d
Your Name <you@example.com>
2024-06-18 14:30:16 +0700

    Remove Front End files from Laravel

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -21,47 +21,37 @@ Route::get('/', function () {
     return view('auth.login');
 });
 
-
-Route::get('/test-mail',function(){
+Route::get('/test-mail', function () {
 
     $message = "Testing mail";
 
     \Mail::raw('Hi, welcome!', function ($message) {
-      $message->to('ajayydavex@gmail.com')
-        ->subject('Testing mail');
+        $message->to('ajayydavex@gmail.com')
+            ->subject('Testing mail');
     });
 
     dd('sent');
-
 });
-
-
-Route::get('/dashboard', function () {
-    return view('front.dashboard');
-})->middleware(['front'])->name('dashboard');
-
-
-require __DIR__.'/front_auth.php';
 
 // Admin routes
 Route::get('/admin/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('admin.dashboard');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
 
 
 
 
 Route::namespace('App\Http\Controllers\Admin')->name('admin.')->prefix('admin')
-    ->group(function(){
-        Route::resource('roles','RoleController');
-        Route::resource('permissions','PermissionController');
-        Route::resource('users','UserController');
-        Route::resource('posts','PostController');
+    ->group(function () {
+        Route::resource('roles', 'RoleController');
+        Route::resource('permissions', 'PermissionController');
+        Route::resource('users', 'UserController');
+        Route::resource('posts', 'PostController');
 
-        Route::get('/profile',[ProfileController::class,'index'])->name('profile');
-        Route::put('/profile-update',[ProfileController::class,'update'])->name('profile.update');
-        Route::get('/mail',[MailSettingController::class,'index'])->name('mail.index');
-        Route::put('/mail-update/{mailsetting}',[MailSettingController::class,'update'])->name('mail.update');
-});
+        Route::get('/profile', [ProfileController::class, 'index'])->name('profile');
+        Route::put('/profile-update', [ProfileController::class, 'update'])->name('profile.update');
+        Route::get('/mail', [MailSettingController::class, 'index'])->name('mail.index');
+        Route::put('/mail-update/{mailsetting}', [MailSettingController::class, 'update'])->name('mail.update');
+    });
