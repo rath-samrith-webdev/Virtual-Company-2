de0d80b098d60037320bb161fa8003caf3e06dd8
Your Name <you@example.com>
2024-06-21 12:00:20 +0700

    Completed Design DB

---
+++ backend/database/migrations/2024_06_21_012451_create_hospitals_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('hospitals', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('category_id')->constrained('categories')->onDelete('cascade');
+            $table->string('street_address')->nullable();
+            $table->string('village')->nullable();
+            $table->string('commune')->nullable();
+            $table->string('district')->nullable();
+            $table->string('province')->nullable();
+            $table->string('latitude')->nullable();
+            $table->string('longitude')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('hospitals');
+    }
+};
