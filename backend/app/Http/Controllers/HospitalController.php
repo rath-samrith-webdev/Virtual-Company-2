88603069c0f57a2cdebf1e2bb886a926b12b378a
Your Name <you@example.com>
2024-06-23 16:33:54 +0700

    [page] Completed design landing page VC2-66

--- backend/app/Http/Controllers/HospitalController.php
+++ backend/app/Http/Controllers/HospitalController.php
@@ -13,7 +13,7 @@ class HospitalController extends Controller
      */
     public function index()
     {
-        //
+        return response()->json(['success'=>true,'data'=>Hospital::all()],200);
     }
 
     /**
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\API\PostController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\HospitalController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -21,6 +22,13 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
+Route::prefix('hospitals')->group(function () {
+    Route::get('/', [HospitalController::class, 'index']);
+    Route::post('/create', [HospitalController::class, 'store']);
+    Route::get('/{hospital}', [HospitalController::class, 'show']);
+    Route::put('/{hospital}', [HospitalController::class, 'update']);
+    Route::delete('/{hospital}', [HospitalController::class, 'destroy']);
+});
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 Route::get('/me', [AuthController::class, 'index'])->middleware('auth:sanctum');
 Route::get('/post/list', [PostController::class, 'index'])->middleware('auth:sanctum');
