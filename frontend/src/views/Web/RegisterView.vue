2399917437297a05c5ca2913fa9ffe900c6bee71
BourKLAN <bour.klan99@gmail.com>
2024-06-27 08:08:19 +0700

    VC2-27 Manage Front-end

--- frontend/src/views/Web/RegisterView.vue
+++ frontend/src/views/Web/RegisterView.vue
@@ -134,6 +134,12 @@
   
   <style scoped>
   
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: "Poppins", sans-serif;
+  }
   body {
     display: flex;
     align-items: center;
