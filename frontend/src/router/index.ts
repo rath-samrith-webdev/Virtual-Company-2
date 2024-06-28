ad075b6cda8cc23dc10708d815d6e631d9fb8fbd
Your Name <you@example.com>
2024-06-28 11:04:13 +0700

    Fix Upload Profile bug By Radit

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -55,7 +55,7 @@ const router = createRouter({
 })
 
 router.beforeEach(async (to, from, next) => {
-  const publicPages = ['/landing', '/login', '/about', '/contact']
+  const publicPages = ['/landing', '/login', '/about', '/contact','/profile']
   const authRequired = !publicPages.includes(to.path)
   const store = useAuthStore()
 
