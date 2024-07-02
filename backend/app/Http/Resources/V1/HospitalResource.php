1a706fe3e4a0528086423089e21ac31b74f6dea8
Your Name <you@example.com>
2024-07-02 09:48:08 +0700

    Fetch Data from back end to display on map

--- backend/app/Http/Resources/V1/HospitalResource.php
+++ backend/app/Http/Resources/V1/HospitalResource.php
@@ -21,6 +21,11 @@ class HospitalResource extends JsonResource
             'preview_images'=>PreviewImagesResource::collection($this->previewImage()->get()),
             'department'=>$this->departments()->get(),
             'appointment'=>AppointmentResource::collection($this->appointments()->get()),
+            'street'=>$this->street?$this->street:'Not Added yet',
+            'village'=>$this->village?$this->village:'Not Added yet',
+            'commune'=>$this->commune?$this->commune:'Not Added yet',
+            'district'=>$this->district?$this->district:'Not Added yet',
+            'province'=>$this->province?$this->province:'Not Added yet',
             'lat'=>$this->latitude,
             'lng'=>$this->longitude,
             'feedbacks'=>RateResource::collection($this->rates()->latest()->get())
