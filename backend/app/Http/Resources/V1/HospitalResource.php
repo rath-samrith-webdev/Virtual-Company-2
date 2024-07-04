f70a92f971b9086737bb7ba2b952e7b588ed9987
PHAL.HIM <PHAL.HIM@PNC.LAN>
2024-07-04 13:33:05 +0700

    update

--- backend/app/Http/Resources/V1/HospitalResource.php
+++ backend/app/Http/Resources/V1/HospitalResource.php
@@ -17,7 +17,7 @@ class HospitalResource extends JsonResource
         return [
             'id'=>$this->id,
             'name'=>$this->name,
-            'cover_image'=>asset('/images/hospital/hospital-cover/hospital-'.$this->id.'/'.$this->cover_image),
+            'cover_image'=>$this->cover_image?asset('/images/hospital/hospital-cover/hospital-'.$this->id.'/'.$this->cover_image):'No cover',
             'preview_images'=>PreviewImagesResource::collection($this->previewImage()->get()),
             'department'=>$this->departments()->get(),
             'appointment'=>AppointmentResource::collection($this->appointments()->get()),
