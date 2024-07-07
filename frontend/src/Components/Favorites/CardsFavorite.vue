892eb4d7553990681443f398b93e2d06e627d536
PHAL.HIM <PHAL.HIM@PNC.LAN>
2024-07-07 09:56:41 +0700

    [ipm] connect back end to front end VC2_49

--- frontend/src/Components/Favorites/CardsFavorite.vue
+++ frontend/src/Components/Favorites/CardsFavorite.vue
@@ -4,18 +4,16 @@
       <div class="wrapper" v-for="cardFavorite in cardFavorites" :key="cardFavorite.id">
         <div class="single-card">
           <div class="img-area">
-            <img v-if="cardFavorite.cover_image == 'No cover'" :src="cardFavorite.cover_image" class="card-img-top"
-              alt="...">
-            <h4 v-if="cardFavorite.cover_image !== 'No cover'">
-              <img src="https://i0.wp.com/sunrisedaycamp.org/wp-content/uploads/2020/10/placeholder.png?ssl=1" alt=""
-                width="400px">
+            <img v-if="cardFavorite.cover_image !== 'No cover'" :src="cardFavorite.cover_image" class="card-img-top" alt="...">
+            <h4 v-else>
+              <img src="https://i0.wp.com/sunrisedaycamp.org/wp-content/uploads/2020/10/placeholder.png?ssl=1" alt="" width="400px">
             </h4>
           </div>
           <div class="info">
             <h5>{{ cardFavorite.name }}</h5>
             <p class="price">{{ cardFavorite.time }}</p>
             <p>{{ cardFavorite.data }}</p>
-            <el-rate v-model="value" disabled show-score text-color="#ff9900" score-template="{value} points" />
+            <el-rate v-model="cardFavorite.rating" disabled show-score text-color="#ff9900" score-template="{value} points" />
           </div>
           <div class="card_button m-2">
             <button type="button" class="btn btn-warning"><i class="fas fa-info-circle"></i> See Details</button>
@@ -23,7 +21,6 @@
         </div>
       </div>
     </div>
-
   </div>
 </template>
 
@@ -46,8 +43,8 @@ export default {
   },
   computed: {
     filteredCards() {
-      return this.cardAddress.filter(card => {
-        const matchesTitle = card.title.toLowerCase().includes(this.searchQuery.toLowerCase());
+      return this.cardFavorites.filter(card => {
+        const matchesTitle = card.name.toLowerCase().includes(this.searchQuery.toLowerCase());
         const matchesOptions = this.selectedOptions.length === 0 || this.selectedOptions.includes(card.address);
         return matchesTitle && matchesOptions;
       });
@@ -56,14 +53,10 @@ export default {
   methods: {
     async userFavorite() {
       try {
-        const { data } = await axiosInstance.get('/hospitals/list')
-        console.log(data.data);
-        data.data.forEach(userFavorite => {
-          this.cardFavorites.push(userFavorite)
-        })
+        const { data } = await axiosInstance.get('/favourites/list')
+        this.cardFavorites = data.data;
       } catch (error) {
         console.log(error)
-        return null
       }
     }
   },
@@ -112,68 +105,27 @@ export default {
   transform: scale(1.1);
 }
 
-.overlay {
-  position: absolute;
-  top: 0;
-  left: 0;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 174, 255, 0.8);
-  opacity: 0;
-  transition: opacity 0.3s ease;
-}
-
-.single-card:hover .overlay {
-  opacity: 1;
-}
-
-.add-to-cart,
-.view-details {
-  background-color: #fff;
-  color: #000000;
-  padding: 10px 15px;
-  border-radius: 4px;
-  font-size: 12px;
-  font-weight: bold;
-  text-transform: uppercase;
-  letter-spacing: 1px;
-  cursor: pointer;
-  transition: background-color 0.3s ease, color 0.3s ease;
-  margin-bottom: 10px;
-}
-
-.add-to-cart:hover,
-.view-details:hover {
-  background-color: orangered;
-  color: #000000;
-  border: none;
-}
-
 .info {
   padding: 10px;
   text-align: center;
   color: #000000;
 }
 
-.info h3 {
+.info h5 {
   margin: 15px 0 10px;
-  font-size: 30px;
+  font-size: 20px;
   font-weight: bold;
   text-transform: uppercase;
-  font-family: 'Bebas Neue', sans-serif;
 }
 
 .info .price {
   margin: 0;
-  font-size: 30px;
+  font-size: 20px;
   font-weight: bold;
   color: rgb(255, 229, 84);
 }
-.card_button{
+
+.card_button {
   display: flex;
   justify-content: flex-end;
 }
