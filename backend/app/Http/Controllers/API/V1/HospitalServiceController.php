0d982110d4b80cb2703691b99e193728cc5a1aca
PHAL.HIM <PHAL.HIM@PNC.LAN>
2024-07-24 09:19:18 +0700

    [imp] create add and delete VC2-147

--- backend/app/Http/Controllers/API/V1/HospitalServiceController.php
+++ backend/app/Http/Controllers/API/V1/HospitalServiceController.php
@@ -41,9 +41,9 @@ class HospitalServiceController extends Controller
         $user=Auth::user();
         $data=$request->validate([
             'name'=>'required',
-            'description'=>'required',
+            'description'=>'string',
             'image'=>'required|image|mimes:jpeg,png,jpg,gif,svg',
-            'hospital_id'=>'required|exists:App\Models\Hospital,id',
+            'hospital_id'=>'exists:App\Models\Hospital,id',
         ]);
         $hospital_id=$user->hospital->id;
         $image=$request->file('image');
@@ -83,40 +83,40 @@ class HospitalServiceController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, HospitalService $hospitalService)
-    {
-        $user=Auth::user();
-        $data=$request->validate([
-            'name'=>'required',
-            'description'=>'required',
-            'image'=>'required|image|mimes:jpeg,png,jpg,gif,svg',
-            'hospital_id'=>'required|exists:App\Models\Hospital,id',
-        ]);
-        $hospital_id=$user->hospital->id;
-        $image=$request->file('image');
-        $ext=$image->getClientOriginalExtension();
-        $filename=time().'.'.$ext;
-        try {
-            if(File::exists(public_path('/').'images/hospital'.$hospitalService->hospital_id.'/service'.$hospitalService->id.'/'.$hospitalService->image)){
-                File::delete(public_path('/').'images/hospital'.$hospitalService->hospital_id.'/service'.$hospitalService->id.'/'.$hospitalService->image);
-            }
-            if(!$user->hasRole('admin')){
-                if($user->hasRole('hospital')){
-                    $data['hospital_id']=$hospital_id;
-                    $data['image']=$filename;
-                    $hospitalService->update($data);
-                    $image->move(public_path('/').'images/hospital'.$hospitalService->hospital_id.'/service'.$hospitalService->id,$filename);
-                    return response()->json(['success'=>true,'message'=>'Service updated successfully.']);
+        public function update(Request $request, HospitalService $hospitalService)
+        {
+            $user=Auth::user();
+            $data=$request->validate([
+                'name'=>'required',
+                'description'=>'required',
+                'image'=>'required|image|mimes:jpeg,png,jpg,gif,svg',
+                'hospital_id'=>'required|exists:App\Models\Hospital,id',
+            ]);
+            $hospital_id=$user->hospital->id;
+            $image=$request->file('image');
+            $ext=$image->getClientOriginalExtension();
+            $filename=time().'.'.$ext;
+            try {
+                if(File::exists(public_path('/').'images/hospital'.$hospitalService->hospital_id.'/service'.$hospitalService->id.'/'.$hospitalService->image)){
+                    File::delete(public_path('/').'images/hospital'.$hospitalService->hospital_id.'/service'.$hospitalService->id.'/'.$hospitalService->image);
+                }
+                if(!$user->hasRole('admin')){
+                    if($user->hasRole('hospital')){
+                        $data['hospital_id']=$hospital_id;
+                        $data['image']=$filename;
+                        $hospitalService->update($data);
+                        $image->move(public_path('/').'images/hospital'.$hospitalService->hospital_id.'/service'.$hospitalService->id,$filename);
+                        return response()->json(['success'=>true,'message'=>'Service updated successfully.']);
+                    }else{
+                        return response()->json(['message'=>'You are not authorized to access this page'],403);
+                    }
                 }else{
                     return response()->json(['message'=>'You are not authorized to access this page'],403);
                 }
-            }else{
-                return response()->json(['message'=>'You are not authorized to access this page'],403);
+            }catch (\Exception $exception){
+                return response()->json(['success'=>false,'message'=>$exception->getMessage()],422);
             }
-        }catch (\Exception $exception){
-            return response()->json(['success'=>false,'message'=>$exception->getMessage()],422);
         }
-    }
 
     /**
      * Remove the specified resource from storage.
